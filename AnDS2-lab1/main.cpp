c1e94b5d8b9b4d60c3e105a0a165e48562d6a81a
KsushaK <ksushak1803@gmail.com>
2024-04-11 19:44:40 +0400

    start do statistics

--- AnDS2-lab1/main.cpp
+++ AnDS2-lab1/main.cpp
@@ -141,3 +141,85 @@ public:
     }
 };
 
+size_t lcg() {
+    static size_t x = 0;
+    x = (1021 * x + 24631) % 116640;
+    return x;
+}
+
+template<typename Function>
+double measureTime(Function&& func, int repetitions) {
+    clock_t start, end;
+    double totalTime = 0;
+
+    for (int i = 0; i < repetitions; ++i) {
+        start = clock();
+        func();
+        end = clock();
+        totalTime += static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    }
+
+    return totalTime / repetitions;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    BinarySearchTree bst;
+    double insertTime, searchTime, eraseTime;
+
+    insertTime = measureTime([&]() {
+        for (int i = 0; i < 100; ++i) { // 100 попыток заполнения
+            bst = BinarySearchTree(); // Сброс дерева перед каждой попыткой
+            for (int j = 0; j < 1000; ++j) {
+                bst.insert(lcg());
+            }
+        }
+        }, 100); // 100 попыток
+
+    searchTime = measureTime([&]() {
+        for (int i = 0; i < 1000; ++i) {
+            bst.contains(lcg());
+        }
+        }, 1000); // 1000 попыток
+
+    eraseTime = measureTime([&]() {
+        for (int i = 0; i < 1000; ++i) {
+            bst.erase(lcg());
+        }
+        }, 1000); // 1000 попыток
+
+    std::cout << "BinarySearchTree: " << std::endl;
+    std::cout << "Average Insertion Time: " << insertTime << " seconds" << std::endl;
+    std::cout << "Average Search Time: " << searchTime << " seconds" << std::endl;
+    std::cout << "Average Erase Time: " << eraseTime << " seconds" << std::endl;
+
+    std::vector<int> vec;
+    insertTime = measureTime([&]() {
+        for (int i = 0; i < 100; ++i) {
+            vec.clear(); // Очищаем вектор перед каждой попыткой
+            for (int j = 0; j < 1000; ++j) {
+                vec.push_back(lcg());
+            }
+        }
+        }, 100); // 100 попыток
+
+    searchTime = measureTime([&]() {
+        for (int i = 0; i < 1000; ++i) {
+            std::find(vec.begin(), vec.end(), lcg());
+        }
+        }, 1000); // 1000 попыток
+
+    eraseTime = measureTime([&]() {
+        for (int i = 0; i < 1000; ++i) {
+            vec.erase(std::remove(vec.begin(), vec.end(), lcg()), vec.end());
+        }
+        }, 1000); // 1000 попыток
+
+    std::cout << "std::vector<int>: " << std::endl;
+    std::cout << "Average Insertion Time: " << insertTime << " seconds" << std::endl;
+    std::cout << "Average Search Time: " << searchTime << " seconds" << std::endl;
+    std::cout << "Average Erase Time: " << eraseTime << " seconds" << std::endl;
+
+    return 0;
+}
